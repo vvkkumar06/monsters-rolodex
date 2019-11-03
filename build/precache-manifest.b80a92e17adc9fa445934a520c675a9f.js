bdbb49fa792db940cacae4804a230b958ca240b1
Vivek Kumar <vivek.kumar@optimusinfo.com>
2019-11-03 19:42:45 +0530

    build folder added

---
+++ build/precache-manifest.b80a92e17adc9fa445934a520c675a9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f39cf413fac7dbe824fb93677a64224b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "64d6d7ae323309c6a6ab",
+    "url": "/monsters-rolodex/static/css/main.715b6cf9.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "64d6d7ae323309c6a6ab",
+    "url": "/monsters-rolodex/static/js/main.877d8723.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
